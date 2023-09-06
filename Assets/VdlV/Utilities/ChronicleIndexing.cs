4aaf436ecd01d85088152e115e5a01c1c761863c
SAMHILLPC\Sam Hill <whatinthesamhill@protonmail.com>
2023-09-05 19:06:33 -0500

    ChronicleIndexing
    remove JointColumnSpec

---
+++ Assets/VdlV/Utilities/ChronicleIndexing.cs
@@ -0,0 +1,13 @@
+﻿using TED.Interpreter;
+using static TED.Tables.IndexMode;
+
+namespace VdlV.Utilities {
+    public static class ChronicleIndexing {
+        public static IColumnSpec<T> DemoteKey<T>(IColumnSpec<T> columnSpec) => 
+            columnSpec.IndexMode == Key ? columnSpec.TypedVariable.Indexed : columnSpec;
+
+        //public static IColumnSpec<T> JointColumnSpec<T>(IColumnSpec<T> columnSpec) {
+        //    columnSpec.TypedVariable
+        //}
+    }
+}
