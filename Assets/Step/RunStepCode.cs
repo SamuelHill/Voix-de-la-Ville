11562122674074e2f04206a0541636f342cb6f17
Samuel Jack Hill <whatinthesamhill@protonmail.com>
2024-05-02 18:26:35 -0500

    Update RunStepCode.cs

--- Assets/Step/RunStepCode.cs
+++ Assets/Step/RunStepCode.cs
@@ -6,18 +6,29 @@ public class RunStepCode : MonoBehaviour {
     private string _death;
     private string _news;
     private string _marriage;
-    private Queue<string> _gossipQueue = new();
+    private readonly Queue<string> _gossipQueue = new();
 
-    // ReSharper disable MemberCanBePrivate.Global, FieldCanBeMadeReadOnly.Global, ConvertToConstant.Global, UnassignedField.Global
+    // ReSharper disable MemberCanBePrivate.Global, FieldCanBeMadeReadOnly.Global, ConvertToConstant.Global, UnassignedField.Global, InconsistentNaming
     public int MaxGossipCount = 10;
     public int MaxStepAttempts = 5;
-    // ReSharper restore UnassignedField.Global, ConvertToConstant.Global, FieldCanBeMadeReadOnly.Global, MemberCanBePrivate.Global
+    // ReSharper restore InconsistentNaming, UnassignedField.Global, ConvertToConstant.Global, FieldCanBeMadeReadOnly.Global, MemberCanBePrivate.Global
 
     private static bool TryRun(string stepString, out string stepOutput) {
         try {
             stepOutput = StepCode.Run(stepString);
         } catch (Step.Interpreter.CallFailedException) {
             stepOutput = "";
+        }
+        return stepOutput != "";
+    }
+    private static bool TryRunCheckIfNew(string stepString, out string stepOutput, string previousStepOutput) {
+        try {
+            stepOutput = StepCode.Run(stepString);
+        } catch (Step.Interpreter.CallFailedException) {
+            stepOutput = "";
+        }
+        if (stepOutput == previousStepOutput) {
+            previousStepOutput = stepOutput;
         }
         return stepOutput != "";
     }
@@ -48,5 +59,5 @@ public class RunStepCode : MonoBehaviour {
 
     public void ShowGossip() => GUI.Label(new Rect(600, 200, 1000, 1000), string.Join("\n", _gossipQueue.ToArray()));
 
-    public void ShowNews() => GUI.Label(new Rect(600, 500, 1000, 1000), _news);
+    public void ShowNews() => GUI.Label(new Rect(600, 400, 1000, 1000), _news);
 }
