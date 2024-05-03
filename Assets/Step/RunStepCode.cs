9e61e44229367f8b9132381ef780535047837616
Samuel Jack Hill <whatinthesamhill@protonmail.com>
2024-05-03 03:08:56 -0500

    Code cleanup on stepcode

--- Assets/Step/RunStepCode.cs
+++ Assets/Step/RunStepCode.cs
@@ -1,12 +1,11 @@
 using UnityEngine;
-using Vldv.Step;
 using System.Collections.Generic;
+using VdlV.Step;
 
 public class RunStepCode : MonoBehaviour {
     private string _death;
     private string _news;
     private string _marriage;
-    private string _previousMarriage;
     private readonly Queue<string> _gossipQueue = new();
 
     // ReSharper disable MemberCanBePrivate.Global, FieldCanBeMadeReadOnly.Global, ConvertToConstant.Global, UnassignedField.Global, InconsistentNaming
@@ -15,37 +14,27 @@ public class RunStepCode : MonoBehaviour {
     // ReSharper restore InconsistentNaming, UnassignedField.Global, ConvertToConstant.Global, FieldCanBeMadeReadOnly.Global, MemberCanBePrivate.Global
 
     private static bool TryRun(string stepString, out string stepOutput) {
-        try {
-            stepOutput = StepCode.Run(stepString);
-        } catch (Step.Interpreter.CallFailedException) {
-            stepOutput = "";
-        }
+        try { stepOutput = StepCode.Run(stepString); } catch (Step.Interpreter.CallFailedException) { stepOutput = ""; }
         return stepOutput != "";
     }
-    
+
     private static bool TryRunCheckIfNew(string stepString, out string stepOutput, ref string previousStepOutput) {
-        try {
-            stepOutput = StepCode.Run(stepString);
-        } catch (Step.Interpreter.CallFailedException) {
-            stepOutput = "";
-        }
+        try { stepOutput = StepCode.Run(stepString); } catch (Step.Interpreter.CallFailedException) { stepOutput = ""; }
         if (stepOutput == previousStepOutput) return false;
         previousStepOutput = stepOutput;
         return stepOutput != "";
     }
 
     public bool PauseOnDeath() => TryRun("Death", out _death);
-    
-    public bool PauseOnMarriage() => TryRunCheckIfNew("Marriage", out _marriage, ref _previousMarriage);
+
+    public bool PauseOnMarriage() => TryRun("Marriage", out _marriage);
 
     public void ProcessGossip() {
         var tryCounter = 0;
         var newGossip = "";
         while (tryCounter <= MaxStepAttempts) {
             newGossip = StepCode.Run("Gossip");
-            if (!_gossipQueue.Contains(newGossip)) {
-                break;
-            }
+            if (!_gossipQueue.Contains(newGossip)) { break; }
             tryCounter++;
         }
         _gossipQueue.Enqueue(newGossip);
@@ -53,7 +42,7 @@ public class RunStepCode : MonoBehaviour {
     }
 
     public void GetNews() => _news = StepCode.Run("Newspaper");
-    
+
     public void ShowDeath() => GUI.Label(new Rect(600, 100, 1000, 1000), _death);
 
     public void ShowMarriage() => GUI.Label(new Rect(600, 150, 1000, 1000), _marriage);
--- Assets/Step/StepCode.cs
+++ Assets/Step/StepCode.cs
@@ -1,73 +1,66 @@
-using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using Step;
 using Step.Interpreter;
 using TED;
-using VdlV.Simulator;
-using VdlV.Utilities;
-using VdlV.ValueTypes;
+using UnityEngine;
 using Random = System.Random;
 
-// ReSharper disable once CheckNamespace
-namespace Vldv.Step
-{
-    public static class StepCode
-    {
-        public static Module Module;
+namespace VdlV.Step {
+    using Simulator;
+    using ValueTypes;
+    using static Time.Clock;
+    using static Simulator.StaticTables;
+    using static Utilities.Randomize;
+    using static ArgumentCountException; // from Step.Interpreter
+    using static Module;                 // from Step
+    using static Application;            // from UnityEngine
+    using static Debug;                  // from UnityEngine
+    using static System.IO.Path;
+    
+    public static class StepCode {
+        private static readonly Module Module;
         private static readonly Random Rng;
 
-        static StepCode()
-        {
-            Module = new Module("VdlV", Module.Global);
-            Module.AddBindHook((Module.BindHook)ImportTedPredicate);
-            Module.LoadDirectory(Path.Combine(UnityEngine.Application.dataPath, "Step"));
-            Rng = Randomize.MakeRng();
-            Module["RomanticInteractions"] = StaticTables.romanticInteractions;
+        static StepCode() {
+            Module = new Module("VdlV", Global);
+            Module.AddBindHook((BindHook)ImportTedPredicate);
+            Module.LoadDirectory(Combine(dataPath, "Step"));
+            Rng = MakeRng();
+            Module["RomanticInteractions"] = romanticInteractions;
             Module["MaleSex"] = Sex.Male;
             Module["FemaleSex"] = Sex.Female;
-            Module["CurrentDate"] = new SimpleFunction<int, string>("CurrentDate", (int _) => VdlV.Time.Clock.Date().ToString());
-            Module["Log"] = new SimplePredicate<object>("Log", (object o) => {UnityEngine.Debug.Log(o); return true;});
+            Module["CurrentDate"] = new SimpleFunction<int, string>("CurrentDate", _ => Date().ToString());
+            Module["Log"] = new SimplePredicate<object>("Log", o => { Log(o); return true; });
         }
 
-        private static IEnumerable<object[]> TableRowsInRandomOrder(TablePredicate predicate)
-        {
+        private static IEnumerable<object[]> TableRowsInRandomOrder(TablePredicate predicate) {
             var buffer = new object[predicate.Arity];
-            var row = (uint)Randomize.Integer((int)predicate.Length, Rng);
-            var stride = (uint)Randomize.RandomPrime(Rng);
+            var row = (uint)Integer((int)predicate.Length, Rng);
+            var stride = (uint)RandomPrime(Rng);
             var length = predicate.Length;
-            for (var i = 0; i < length; i++)
-            {
+            for (var i = 0; i < length; i++) {
                 predicate.GetUntypedRowData(row % length, buffer);
                 yield return buffer;
                 row += stride;
             }
         }
 
-        private static bool ImportTedPredicate(StateVariableName variableName, out object stepTask)
-        {
+        private static bool ImportTedPredicate(StateVariableName variableName, out object stepTask) {
             var name = variableName.Name;
             var table = VoixDeLaVille.Simulation.Tables.FirstOrDefault(t => t.Name == name);
-
-            if (table != null)
-            {
-                stepTask = new GeneralPrimitive(name, (args, output, env, frame, k) =>
-                {
+            if (table != null) {
+                stepTask = new GeneralPrimitive(name, (args, output, env, frame, k) => {
                     // ReSharper disable once CoVariantArrayConversion
-                    ArgumentCountException.Check(name, table.Arity, table.DefaultVariables);
+                    Check(name, table.Arity, table.DefaultVariables);
                     foreach (var tuple in TableRowsInRandomOrder(table))
-                    {
                         if (env.UnifyArrays(args, tuple, out BindingList u) &&
                             k(output, u, env.State, frame))
                             return true;
-                    }
-
                     return false;
                 });
                 return true;
             }
-
             stepTask = null;
             return false;
         }
